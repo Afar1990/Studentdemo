021b7efb8805bf196bfc9b19c8e49546b4948e76
Afar1990 <133712683+Afar1990@users.noreply.github.com>
2023-05-19 14:33:54 -0400

    hello

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -13,18 +13,31 @@ package ca.sheridancollege.week1.softwarefundamentals;
  */
 public class StudentList {
     
-    public static void main(String[] args)
-    {
-       Student [] studentList = new Student[1];//change size before running
+    public static void main(String[] args) {
+       Student[] studentList = new Student[4];
        Student s1 = new Student();
        s1.setName("Liz Dancy");
-       studentList[0]=s1;
-       //fill in 4 student names before running
-       for(Student s: studentList)
-       {
+       studentList[0] = s1;
+       
+       Student s2 = new Student();
+       s2.setName("Jack");
+       studentList[1] = s2;
+       
+       Student s3 = new Student();
+       s3.setName("Mack");
+       studentList[2] = s3;
+       
+       Student s4 = new Student();
+       s4.setName("Athif");
+       studentList[3] = s4;
+       
+       Student s5 = new Student();
+       s5.setName("Mike");
+       studentList[4] = s5;
+       
+       for (Student s : studentList) {
            System.out.println(s.getName());
        }
-        
     }
     
 }
